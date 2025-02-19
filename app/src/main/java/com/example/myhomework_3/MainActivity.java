2df7aac6309ec2449eb29f39f11061b45d53da61
akbaraktanuulu <akbaraktan@icloud.com>
2025-02-19 19:05:09 +0600

    fix

--- app/src/main/java/com/example/myhomework_3/MainActivity.java
+++ app/src/main/java/com/example/myhomework_3/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.myhomework_3;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -18,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private int firstOperand, secondOperand;
     private boolean is0perationClich;
+
+    private Button button;
     String operator = "";
 
     @SuppressLint("MissingInflatedId")
@@ -29,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.btn_open);
+        button.setOnClickListener(view -> {
+            Bundle bunble = new Bundle();
+            bunble.putString("RESULT", textView.getText().toString());
+            Intent intent = new Intent(this,SecondActivity.class);
+            intent.putExtra("BUDLE", bunble);
+            startActivity(intent);
+            finish();
+        });
+
+
         // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
         //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
         //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -36,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         //  });
         textView = findViewById(R.id.text_view);
     }
-
 
     public void onNamberClick(View view) {
 
@@ -58,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
     public void onOperationClick(View view) {
         Button button = (Button) view;
         if (view.getId() != R.id.btn_equal) {
-           firstOperand = Integer.parseInt(textView.getText().toString());
-           operator = button.getText().toString();
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = button.getText().toString();
         } else {
             secondOperand = Integer.parseInt(textView.getText().toString());
             // 33         12             21
@@ -77,11 +86,12 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 default:
                     result = firstOperand - secondOperand;
-          }
-          textView.setText(String.valueOf(result));
+            }
+            textView.setText(String.valueOf(result));
 
         }
 
         is0perationClich = true;
     }
+
 }
